8054e3bbbdfc4abab0eef0ba415049c4cbe0b585
villasv <villasv@outlook.com>
2016-05-31 16:15:38 -0400

    python3 setup

---
+++ setup.py
@@ -0,0 +1,28 @@
+import sys
+from distutils.core import setup
+
+
+VERSION = '0.0.0'
+PY3 = sys.version_info[0] >= 3
+
+setup(
+    name='plutonium',
+    version=VERSION,
+    description='Device driver library of sensors',
+    author='Victor Villas',
+    author_email='villasv@outlook.com',
+    license='GPLv3+',
+    url='https://github.com/villasv/Plutonium',
+    packages=['plutonium'],
+    long_description=open('README.md').read() + open('CHANGELOG.md').read(),
+    classifiers=[
+        "License :: OSI Approved :: GNU Affero General Public License v3 or "
+        "later (AGPLv3+)",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 2",
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    keywords='plutonium raspberrypi sensor',
+)
