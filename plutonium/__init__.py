a05b8949a346719bededc10ba627163732572c01
villasv <villasv@outlook.com>
2016-06-03 10:39:34 -0400

    proxy setup at monitor thread

--- plutonium/__init__.py
+++ plutonium/__init__.py
@@ -1,1 +1,27 @@
-from plutonium.plugnplay import accelerometer, thermometer
+from plutonium.devices.DeviceProxy import DeviceProxy
+import threading
+import time
+from plutonium.common.utils import i2c_addresses
+from plutonium.devices.mapping import driver_map
+
+DEVICE_CHECK_INTERVAL = 5  # seconds
+DEVICES = {}
+
+accelerometer = DeviceProxy()
+
+
+def monitor():
+    while True:
+        addresses = i2c_addresses()
+        for addr in addresses:
+            if addr not in DEVICES.keys():
+                device = driver_map[addr]()
+                DEVICES[addr] = device
+                if 'accelerometer' in device.sensors:
+                    accelerometer.set_device(device)
+        time.sleep(DEVICE_CHECK_INTERVAL)
+    pass
+
+monitor_thread = threading.Thread(target=monitor)
+monitor_thread.daemon = True
+monitor_thread.start()
--- plutonium/devices/DeviceProxy.py
+++ plutonium/devices/DeviceProxy.py
@@ -5,10 +5,15 @@ class DeviceProxy:
     def __init__(self, device: Device = None):
         self._device = device
 
+    def set_device(self, device: Device):
+        object.__setattr__(self, '_device', device)
+
+    def no_op(self):
+        pass
+
     def __getattribute__(self, name):
         _device = object.__getattribute__(self, '_device')
         if _device is not None:
             return getattr(self._device, name)
         else:
-            print("Device not found")
-            return None
+            raise AttributeError()
