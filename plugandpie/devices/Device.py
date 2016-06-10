b2101b2c7e214dd3995ded82adea14a3d44c14cf
Victor Villas <villasv@outlook.com>
2016-06-10 19:31:53 -0400

    registers inside device.register dictionary

--- plugandpie/devices/Device.py
+++ plugandpie/devices/Device.py
@@ -1,10 +1,17 @@
-from plugandpie.interfaces.Interface import Interface
+""" This module contains the Device class
+"""
 
 
 class Device(object):
+    """ Class representing any kind of device.
+    The device should have a list of sensor tags that define its purpose,
+    an underlying interface of communication and the dictionary of registers.
+
+    """
     def __init__(self, sensors, interface):
         self.sensors = sensors
         self.interface = interface
+        self.register = {}
 
     def standby(self):
         raise NotImplementedError()
